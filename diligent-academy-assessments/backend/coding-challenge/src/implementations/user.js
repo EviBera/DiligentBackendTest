0eab6d28b4c63ce25fcc215bd0c68e912f506876
EviBera <beraevi@gmail.com>
2024-08-21 10:07:30 +0200

    Implement required methods in User class

--- diligent-academy-assessments/backend/coding-challenge/src/implementations/user.js
+++ diligent-academy-assessments/backend/coding-challenge/src/implementations/user.js
@@ -4,18 +4,55 @@ export class User {
     }
 
     borrow(book, library) {
-        throw new Error('Not implemented');
+        try {
+            let booksInLibrary = library.getBooks();
+
+            if (booksInLibrary.indexOf(book) === -1) {
+                console.log("The book is not in the library.");
+            } else {
+                this.books.push(book);
+                library.removeBook(book);
+            }
+        }
+        catch (err) {
+            console.error("Unexpected error: " + err);
+        }
     }
 
     hasBook(book) {
-        throw new Error('Not implemented');
+        try {
+            return this.books.indexOf(book) >= 0;
+        }
+        catch (err) {
+            console.error("Unexpected error: " + err);
+        }
     }
 
     return(book, library) {
-        throw new Error('Not implemented');
+        try {
+            if (this.books.indexOf(book) < 0) {
+                return false;
+            } else {
+                this.books = this.books.filter(b => b !== book);
+                library.addBook(book);
+                return true;
+            }
+        }
+        catch (err) {
+            console.error("Unexpected error: " + err);
+        }
     }
 
     getBookNames() {
-        throw new Error('Not implemented');
+        try {
+            let bookNames = [];
+            this.books.forEach(book => {
+                bookNames.push(book.name);
+            });
+            return bookNames;
+        }
+        catch (err) {
+            console.error("Unexpected error: " + err);
+        }
     }
 }
